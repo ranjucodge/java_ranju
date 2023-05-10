8696d3e54d6729028b5533ba0ce8c03f00a0630d
DELL <rk.codge990@gmail.com>
2023-05-10 10:55:09 -0700

    First unaryoperator1

---
+++ src/unaryoperator1.java
@@ -0,0 +1,11 @@
+public class unaryoperator1 {
+    public static void main(String[]args){
+        int a=3;
+        float b=1.1f;
+        System.out.println(++b);
+        System.out.println(b);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a);
+    }
+}
